297e64591c59ac78238fd18c339d019201c5ce87
Roman Bratchykov <brathcykov.roman@chnu.edu.ua>
2025-09-13 22:26:25 +0300

    Added classes Person and Family

---
+++ Lab_3(cs)/Lab_3.cs
@@ -0,0 +1,16 @@
+﻿using Lab_3_cs_;
+using System;
+using System.Collections.Generic;
+
+class Lab_3
+{
+    Person person1 = new Person("Pesho", 20);
+    Person person2 = new Person("Gosho", 18);
+    Person person3 = new Person("Stamat", 43);
+    Person person4 = new Person();
+    Person person5 = new Person()
+    {
+        Name = "Ivan",
+        Age = 19
+    };
+}
---
+++ Lab_3(cs)/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_3_cs_
+{
+    internal class Person
+    {
+        private string? name;
+        private int age;
+        public string? Name {get; set; }
+        public int Age { get; set; }
+        public Person(string? name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+        public Person()
+        {
+            this.Name = "No name";
+            this.Age = 1;
+        }
+        public Person (int age)
+        {
+            this.Name = "No name";
+            this.Age = age;
+        }
+    }
+}
