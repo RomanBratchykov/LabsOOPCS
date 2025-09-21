fb1465ecb1e112c8e73d6271655bc04a182acd5a
Roman Bratchykov <brathcykov.roman@chnu.edu.ua>
2025-09-21 14:06:52 +0300

    created classes for automobile and engine

---
+++ Lab_3(cs)/Automobile.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_3_cs_
+{
+    internal class Automobile
+    {
+        string model;
+        Engine engine;
+        decimal weight;
+        string color;
+        public string Model { get; set; }
+        public Engine Engine { get; set; }
+        public decimal Weight { get; set; }
+        public string Color { get; set; }
+        public Automobile(string model, Engine engine, decimal weight, string color)
+        {
+            Model = model;
+            Engine = engine;
+            Weight = weight;
+            Color = color;
+        }
+        public Automobile()
+        {
+            Model = "No model";
+            Engine = new Engine();
+            Weight = 0;
+            Color = "n/a";
+        }
+        public Automobile(string model, Engine engine)
+        {
+            Model = model;
+            Engine = engine;
+            Weight = 1000;
+            Color = "n/a";
+        }
+    }
+}
---
+++ Lab_3(cs)/Engine.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_3_cs_
+{
+    internal class Engine
+    {
+        string model;
+        decimal power;
+        decimal volume;
+        string efficiency;
+        public string Model { get; set; }
+        public decimal Power { get; set; }
+        public decimal Volume { get; set; }
+        public string Efficiency { get; set; }
+        public Engine(string model, decimal power, decimal volume, string efficiency)
+        {
+            Model = model;
+            Power = power;
+            Volume = volume;
+            Efficiency = efficiency;
+        }
+        public Engine()
+        {
+            Model = "No model";
+            Power = 0;
+            Volume = 0;
+            Efficiency = "n/a";
+        }
+        public Engine(string model, decimal power)
+        {
+            Model = model;
+            Power = power;
+            Volume = 0;
+            Efficiency = "n/a";
+        }
+    }
+}
