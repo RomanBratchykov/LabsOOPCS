121e3daa6e7e83b145d5ee9276e2dbb7e67280ca
Roman Bratchykov <brathcykov.roman@chnu.edu.ua>
2025-09-21 13:56:58 +0300

    Completed task 5

---
+++ Lab_3(cs)/Car.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_3_cs_
+{
+    internal class Car
+    {
+        string model;
+        decimal fuelAmount;
+        decimal fuelConsumptionPerKm;
+        decimal distanceTraveled;
+        public string Model { get; set; }
+        public decimal FuelAmount { get; set; }
+        public decimal FuelConsumptionPerKm { get; set; }
+        public decimal DistanceTraveled { get; set; }
+        public Car( string model, decimal fuelAmount, decimal fuelConsumptionPerKm)
+        {
+            Model = model;
+            FuelAmount = fuelAmount;
+            FuelConsumptionPerKm = fuelConsumptionPerKm;
+            DistanceTraveled = 0;
+        }
+        public Car()
+        {
+            Model = "No model";
+            FuelAmount = 0;
+            FuelConsumptionPerKm = 0;
+            DistanceTraveled = 0;
+        }
+
+        public bool canDrive(decimal distance)
+        {
+            decimal neededFuel = distance * FuelConsumptionPerKm;
+            if (neededFuel <= FuelAmount)
+            {
+                FuelAmount -= neededFuel;
+                DistanceTraveled += distance;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        } 
+        public void showCar()
+        {
+            Console.WriteLine($"{Model} {FuelAmount:F2} {DistanceTraveled}");
+        }
+    }
+}
--- Lab_3(cs)/Lab_3.cs
+++ Lab_3(cs)/Lab_3.cs
@@ -59,6 +59,11 @@ class Lab_3
                     {
                         Console.WriteLine("Enter name and age:");
                         string[] input = Console.ReadLine().Split();
+                        if (input is null || input.Length <= 0)
+                        {
+                            Console.WriteLine("Invalid input");
+                            return;
+                        }
                         string name = input[0];
                         int age = int.Parse(input[1]);
                         Person person = new Person(name, age);
@@ -75,6 +80,11 @@ class Lab_3
                     for (int i = 0; i < n; i++)
                     {
                         string[] input = Console.ReadLine().Split();
+                        if (input is null || input.Length < 4)
+                        {
+                            Console.WriteLine("Invalid input");
+                            return;
+                        }
                         string name = input[0];
                         decimal salary = decimal.Parse(input[1]);
                         string position = input[2];
@@ -119,7 +129,70 @@ class Lab_3
                 break;
              case 5:
                 {
-                }
+                    Console.WriteLine("Enter number of cars:");
+                    int num = int.Parse(Console.ReadLine());
+                    List<Car> cars = new List<Car>();
+                    for (int i = 0; i < num; i++)
+                    {
+                        Console.WriteLine("Enter car " + (i + 1) + " (example AudiA4(model) 23(fuelAmount) 0.3(usage of fuel per km) )" );
+                        cars.Add(new Car());
+                        string[] input = Console.ReadLine().Split();
+                        if (input is null || input.Length <= 0)
+                        {
+                            Console.WriteLine("Invalid input");
+                            return;
+                        }
+                        foreach (var car in cars)
+                        {
+                            if (car.Model == input[0])
+                            {
+                                Console.WriteLine("Car already exists");
+                                i--;
+                                continue;
+                            }
+                        }
+                        cars[i].Model = input[0];
+                        cars[i].FuelAmount = decimal.Parse(input[1]);
+                        cars[i].FuelConsumptionPerKm = decimal.Parse(input[2]);
+                    }
+                    Console.WriteLine("Enter commands (Drive {carModel} {amountOfKm}) or End to stop:");
+                    while (true)
+                    {
+                        string[] command = Console.ReadLine().Split();
+                        if (command is null || command.Length <= 0)
+                        {
+                            Console.WriteLine("Invalid input");
+                            return;
+                        }
+                        if (command[0] == "End")
+                        {
+                            foreach (var car in cars)
+                            {
+                                car.showCar();
+                            }
+                            break;
+                        }
+                        string model = command[1];
+                        decimal distance = decimal.Parse(command[2]);
+                        bool found = false;
+                        foreach (var car in cars)
+                        {
+                            if (car.Model == model)
+                            {
+                                found = true;
+                                if (!car.canDrive(distance))
+                                {
+                                    Console.WriteLine("Insufficient fuel for the drive");
+                                }
+                                break;
+                            }
+                        }
+                        if (!found)
+                            {
+                                Console.WriteLine("Car not found");
+                            }
+                        }
+                    }
                 break;
               case 6:
                 {
