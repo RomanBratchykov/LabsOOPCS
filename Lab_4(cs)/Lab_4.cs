7ef2bc55aa8c9fcfb7d8f11b07826755d4a4fe8c
Roman Bratchykov <brathcykov.roman@chnu.edu.ua>
2025-09-24 21:36:17 +0300

    Completed first task

---
+++ Lab_4(cs)/Lab_4.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+
+namespace Lab_4_cs_;
+static public class Lab_4
+{
+    static public void Main()
+    {
+        Console.WriteLine("Choose your task 1-4, 0 for exit");
+        int task = int.Parse(Console.ReadLine());
+        switch (task)
+        {
+            case 1:
+                Console.WriteLine("Enter coordinates (Example: 1 2 6 8)");
+                string[] input = Console.ReadLine().Split(' ');
+                int x1 = int.Parse(input[0]);
+                int y1 = int.Parse(input[1]);
+                Point topLeft = new Point(x1, y1);
+                int x2 = int.Parse(input[2]);
+                int y2 = int.Parse(input[3]);
+                Point bottomRight = new Point(x2, y2);
+                Rectangle rect = new Rectangle(topLeft, bottomRight);
+                Console.WriteLine("Enter number of points");
+                int numberOfPoints= int.Parse(Console.ReadLine());
+                for (int i = 0; i < numberOfPoints; i++)
+                {
+                    Console.WriteLine("Enter coordinates of point (Example: 3 4)");
+                    string[] pointInput = Console.ReadLine().Split(' ');
+                    int px = int.Parse(pointInput[0]);
+                    int py = int.Parse(pointInput[1]);
+                    Point point = new Point(px, py);
+                    if (rect.Contains(point))
+                    {
+                        Console.WriteLine("Point is inside the rectangle");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Point is outside the rectangle");
+                    }
+                }
+                break;
+            case 2:
+                {
+
+                }
+                break;
+            case 3:
+                {
+
+                }
+                break;
+            case 4:
+                {
+
+                }
+                break;
+            case 0:
+                break;
+            default:
+                Console.WriteLine("Wrong task number");
+                break;
+        }
+    }
+}
---
+++ Lab_4(cs)/Rectangle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_4_cs_
+{
+    internal class Rectangle
+    {
+        private Point topLeft;
+        private Point bottomRight;
+        public Point TopLeft
+        {
+            get { return topLeft; }
+            set { topLeft = value; }
+        }
+        public Point BottomRight
+        {
+            get { return bottomRight; }
+            set { bottomRight = value; }
+        }
+        public Rectangle(Point topLeft, Point bottomRight)
+        {
+            this.topLeft = topLeft;
+            this.bottomRight = bottomRight;
+        }
+        public bool Contains(Point point)
+        {
+            return point.x >= topLeft.x && point.x <= bottomRight.x && point.y >= topLeft.y && point.y <= bottomRight.y;
+        }
+    }
+}
