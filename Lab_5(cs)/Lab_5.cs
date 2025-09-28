db7eb645cae4d1b904a575a96992ea9bf565921f
Roman Bratchykov <brathcykov.roman@chnu.edu.ua>
2025-09-28 21:31:00 +0300

    created classes Product and Person

--- Lab_5(cs)/Lab_5.cs
+++ Lab_5(cs)/Lab_5.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Lab_5;
+
+public class Lab_5
+{
+    public static void Main(string[] args)
+    {
+        Console.BackgroundColor = ConsoleColor.Black;
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("Choose task 2-3, 0 for exit");
+        int task = int.Parse(Console.ReadLine());
+        switch (task)
+        {
+            case 2:
+                {
+
+                }
+                break;
+            case 3:
+                { 
+                }
+                break;
+            case 0:
+                break;
+            default:
+                Console.WriteLine("Wrong task number");
+                break;
+        }
+
+    }
+}
---
+++ Lab_5(cs)/Person.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_5_cs_
+{
+    internal class Person
+    {
+        private string name;
+        private decimal money;
+        private List<Product> products;
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Name cannot be empty");
+                }
+                name = value;
+            }
+        }
+        public decimal Money
+        {
+            get
+            {
+                return money;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Money cannot be negative");
+                }
+                money = value;
+            }
+        }
+        public List<Product> Products
+        {
+            get
+            {
+                return products;
+            }
+            set
+            {
+                products = value;
+            }
+        }
+        public Person(string name, decimal money)
+        {
+            Name = name;
+            Money = money;
+            Products = new List<Product>();
+        }
+        public void buyProduct(Product product)
+        {
+            if (Money >= product.Cost)
+            {
+                Money -= product.Cost;
+                Products.Add(product);
+                Console.WriteLine($"{Name} bought {product.Name}");
+            }
+            else
+            {
+                Console.WriteLine($"{Name} can't afford {product.Name}");
+            }
+        }
+        public void showProducts()
+        {
+            if (Products == null || Products.Count == 0)
+            {
+                Console.WriteLine($"{Name} hasn`t bought anything");
+            }
+            else
+            {
+                Console.WriteLine($"{Name} - {string.Join(", ", Products.Select(p => p.Name))}");
+            }
+        }
+    }
+}
