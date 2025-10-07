9b66aa6c0e9d73a01076df12b245c569bcdfae9a
Roman Bratchykov <brathcykov.roman@chnu.edu.ua>
2025-10-07 19:00:17 +0300

    completed task 1

---
+++ Lab_7(cs)/Lab_7.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace Lab_7;
+
+internal class Lab_7
+{
+    private static void Main(string[] args)
+    {
+        
+        while (true)
+        {
+            Console.WriteLine("Enter number of task 1-3, 0 for exit");
+            int input = int.Parse(Console.ReadLine());
+            if (input == 0) break;
+            switch (input)
+            {
+                case 0:
+                    return;
+                case 1:
+                    {
+                        Console.WriteLine("Enter model of mobile phone");
+                        string model = Console.ReadLine();
+                        MobileProduction mobile = new MobileProduction(model);
+                        Console.WriteLine("Enter phone numbers to call");
+                        List<ICallable> callables = new List<ICallable>();
+                        string inputNum = Console.ReadLine();
+                        string[] phoneNumbers = inputNum.Split(' ');
+                        foreach (var number in phoneNumbers)
+                        {
+                            try
+                            {
+                                mobile.Call(number);
+                            }
+                            catch (ArgumentException ex)
+                            {
+                                Console.WriteLine(ex.Message);
+                            }
+                        }
+                        Console.WriteLine("Enter URLs to watch");
+                        List<IWatchable> watchables = new List<IWatchable>();
+                        string inputUrl = Console.ReadLine();
+                        string[] urls = inputUrl.Split(' ');
+                        foreach (var url in urls)
+                        {
+                            try
+                            {
+                                mobile.Watch(url);
+                            }
+                            catch (ArgumentException ex)
+                            {
+                                Console.WriteLine(ex.Message);
+                            }
+                        }
+                    }
+                    break;
+                case 2:
+                    throw new NotImplementedException();
+                    break;
+                case 3:
+                    throw new NotImplementedException();
+                    break;
+                default:
+                    Console.WriteLine("Invalid input");
+                    break;
+            }
+        }
+    }
+}
---
+++ Lab_7(cs)/MobileProduction.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_7
+{
+    internal interface ICallable
+    {
+       public void Call(string number);
+    }
+    internal interface IWatchable
+    {
+       public void Watch(string video);
+    }
+    internal class MobileProduction : ICallable, IWatchable
+    {
+        string model = string.Empty;
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+        public MobileProduction(string model)
+        {
+            Model = model;
+        }
+        public void Call(string number)
+        {
+            foreach (char c in number)
+            {
+                if (!char.IsDigit(c))
+                {
+                    throw new ArgumentException("Invalid phone number");
+                }
+            }
+            Console.WriteLine($"Calling {number}...");
+        }
+        public void Watch(string video)
+        {
+            foreach (var c in video)
+            {
+                if (char.IsDigit(c))
+                {
+                    throw new ArgumentException("Invalid URL");
+                }
+            }
+            Console.WriteLine($"Watching {video}...");
+        }
+
+    }
+}
