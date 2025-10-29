82efee538f0b27a202df3e8f97f43d787c399c97
Roman Bratchykov <brathcykov.roman@chnu.edu.ua>
2025-10-29 21:42:12 +0200

    Completed task 2

---
+++ Lab_12(cs)/King.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_12_cs_
+{
+    public delegate void KingAttackedEventHandler();
+    public class Unit
+    {
+        public string Name { get; set; }
+        public bool IsAlive { get; set; } = true;
+        public Unit(string name)
+        {
+            Name = name;
+        }
+        public virtual void Die()
+        {
+            IsAlive = false;
+        }
+        public virtual void RespondToAttack() { }
+
+    }
+    internal class King : Unit
+    {
+        public event KingAttackedEventHandler KingAttacked;
+
+        public King(string name) : base(name)
+        {
+            Name = name;
+        }
+        public void BeAttacked()
+        {
+            Console.WriteLine("The king is being attacked!");
+            OnKingAttacked();
+        }
+        protected virtual void OnKingAttacked()
+        {
+            KingAttacked?.Invoke();
+        }
+        public override void RespondToAttack() { }
+    }
+    internal class RoyalGuard : Unit
+    {
+        public RoyalGuard(string name) : base(name)
+        {
+            Name = name;
+        }
+        public override void RespondToAttack()
+        {
+            if (IsAlive)
+            {
+                Console.WriteLine($"Royal Guard {Name} is defending the king!");
+            }
+        }
+        public override void Die()
+        {
+            IsAlive = false;
+            Console.WriteLine($"Royal Guard {Name} has died!");
+        }
+    }
+    internal class Footman : Unit
+    {
+        public Footman(string name) : base(name)
+        {
+            Name = name;
+        }
+        public override void RespondToAttack()
+        {
+            if (IsAlive)
+            {
+                Console.WriteLine($"Footman {Name} is in panic!");
+            }
+        }
+        public override void Die()
+        {
+            IsAlive = false;
+            Console.WriteLine($"Footman {Name} has died!");
+        }
+    }
+}
--- Lab_12(cs)/Lab12.cs
+++ Lab_12(cs)/Lab12.cs
@@ -27,7 +27,58 @@ namespace Lab_12_cs_
                     }
                     break;
                 case 2:
-                    // Task2(); // Implement Task2 if needed
+                    {
+                        Console.WriteLine("Enter king name");
+                        string kingName = Console.ReadLine();
+                        King king = new King(kingName);
+                        Console.WriteLine("Enter royal guards names with space separator");
+                        string[] royalGuardsNames = Console.ReadLine().Split(' ');
+                        List<Unit> units = new List<Unit>();
+                        foreach (var rgName in royalGuardsNames)
+                        {
+                            RoyalGuard rg = new RoyalGuard(rgName);
+                            units.Add(rg);
+                            king.KingAttacked += rg.RespondToAttack;
+                        }
+                        Console.WriteLine("Enter footman names with space separator");
+                        string[] footmanNames = Console.ReadLine().Split(' ');
+                        foreach (var fmName in footmanNames)
+                        {
+                            Footman fm = new Footman(fmName);
+                            units.Add(fm);
+                            king.KingAttacked += fm.RespondToAttack;
+                        }
+                        Console.WriteLine("Enter commands: attack king/ kill [name of unit]/end");
+                        while (true)
+                        {
+                            string command = Console.ReadLine();
+                            if (command.ToLower() == "end")
+                                break;
+                            else if (command.ToLower() == "attack king")
+                            {
+                                king.BeAttacked();
+                            }
+                            else if (command.ToLower().StartsWith("kill "))
+                            {
+                                string unitNameToKill = command.Substring(5);
+                                Unit unitToKill = units.FirstOrDefault(u => u.Name == unitNameToKill && u.IsAlive);
+                                if (unitToKill != null)
+                                {
+                                    unitToKill.Die();
+                                    king.KingAttacked -= unitToKill.RespondToAttack;
+                                    units.Remove(unitToKill);
+                                }
+                                else
+                                {
+                                    Console.WriteLine($"No alive unit found with the name {unitNameToKill}");
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine("Invalid command");
+                            }
+                        }
+                    }
                     break;
                 case 3:
                     // Task3(); // Implement Task3 if needed
