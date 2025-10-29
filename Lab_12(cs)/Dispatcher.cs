26a654b068039f9ed6e8bf141d26b590503d5c5b
Roman Bratchykov <brathcykov.roman@chnu.edu.ua>
2025-10-29 20:02:25 +0200

    completed task 1

---
+++ Lab_12(cs)/Dispatcher.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Lab_12_cs_
+{
+    public delegate void NameChangeEventHandler(object o, NameChangeEventArgs eventHandler);
+
+    public class NameChangeEventArgs : EventArgs
+    {
+        public  string Name { get; private set; }
+        public  NameChangeEventArgs(string name)
+        {
+            Name = name;
+        }
+    }
+
+    internal class Dispatcher
+    {
+        private string name;
+
+        public event NameChangeEventHandler NameChange;
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (name != value)
+                {
+                    OnNameChange(new NameChangeEventArgs(value));
+                    name = value;
+                }
+            }
+        }
+        public virtual void OnNameChange(NameChangeEventArgs args)
+        {
+            NameChange.Invoke(this, args);
+        }
+    }
+    public class Handler
+    {
+        public void OnNameChange(object o, NameChangeEventArgs args)
+        {
+            Console.WriteLine($"Name changed to: {args.Name}");
+        }
+    }
+}
---
+++ Lab_12(cs)/Lab12.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Lab_12_cs_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number of task 1-4, 0 for exit");
+            int input = int.Parse(Console.ReadLine());
+            switch (input)
+            {
+                case 1:
+                    {
+                        Dispatcher dispatcher = new Dispatcher();
+                        Handler handler = new Handler();
+                        dispatcher.NameChange += handler.OnNameChange;
+                        while (true)
+                        {
+                            Console.Write("Enter a new name (or 'end' to quit): ");
+                            string name = Console.ReadLine();
+                            if (name.ToLower() == "end")
+                                break;
+                            dispatcher.Name = name;
+                        }
+
+                    }
+                    break;
+                case 2:
+                    // Task2(); // Implement Task2 if needed
+                    break;
+                case 3:
+                    // Task3(); // Implement Task3 if needed
+                    break;
+                case 4:
+                    // Task4(); // Implement Task4 if needed
+                    break;
+                case 0:
+                    return;
+                default:
+                    Console.WriteLine("Invalid input");
+                    break;
+            }
+        }
+    }
+}
